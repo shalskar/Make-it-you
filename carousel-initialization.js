8ea2c981238618b98793077224024aa693616131
shalskar <vincent.tetau@gmail.com>
2015-06-05 21:26:49 +1200

    Design updates
    - New colour scheme
    - Added subtle patterns to panels
    - Made header smaller
    - Added image panel

---
+++ carousel-initialization.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $('.wedding-slick').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        speed: 200,
+        responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 3
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 1
+              }
+          }
+        ]
+    });
+
+});
