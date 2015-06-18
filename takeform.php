a30953353c93ebf93624465485f3df5e775cd85f
shalskar <vincent.tetau@gmail.com>
2015-06-19 10:03:17 +1200

    Website launched
    Final updates before I launched the website:
    - favicons
    - content changes
    - cleaned up unnecessary clutter
    
    Address is: www.makeityou.co.nz

--- takeform.php
+++ takeform.php
@@ -1,14 +1,28 @@
-﻿$errors = '';
-$myemail = 'yourname@website.com';//<-----Put Your email address here.
+<?php
+
+ob_start();
+
+$errors = '';
+$myemail = 'rachel@makeityou.co.nz';
+
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
+   empty($_POST['subject'])||
    empty($_POST['message']))
 {
     $errors .= "\n Error: all fields are required";
 }
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
+$subject = $_POST['subject'];
+$cardtype = $_POST['cardtype'];
+$promo = $_POST['promo'];
+$cardselected = $_POST['cardselected'];
 $message = $_POST['message'];
+
+
+
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
 $email_address))
@@ -18,14 +32,20 @@ $email_address))
 
 if( empty($errors))
 {
-$to = $myemail;
-$email_subject = "Contact form submission: $name";
-$email_body = "You have received a new message. ".
-" Here are the details:\n Name: $name \n ".
-"Email: $email_address\n Message \n $message";
-$headers = "From: $myemail\n";
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-//redirect to the 'thank you' page
-header('Location: contact-form-thank-you.html');
+	$to = $myemail;
+	$email_subject = "Make it you enquiry submission: $name";
+	$email_body = "You have received a new enquiry. ".
+	" Here are the details:\n \n Name: $name \n ".
+	"Email: $email_address\n Subject: $subject \n Card type: $cardtype \n Promo code: $promo \n Card: $cardselected \n \n Message: \n $message";
+
+	
+		//redirect to the 'thank you' page
+	header('Location: thankyou.html');
+
+	$headers = "From: $myemail\n";
+	$headers .= "Reply-To: $email_address";
+	mail($to,$email_subject,$email_body,$headers);
+} else
+{
+	echo($errors);
 }
